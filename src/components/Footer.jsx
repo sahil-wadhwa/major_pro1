9df158e27d85fd0011b6090b63ea6cbb6e66259d
Shiv Rai <sshaktirai@gmail.com>
2024-07-19 20:04:04 +0530

    modified footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,12 +9,12 @@ const Footer = () => {
     const github_link="";    
    
     return (
-        <section className="py-10 bg-gray-50 sm:pt-16 lg:pt-24">
-    <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-        <div className="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12">
-            <div className="col-span-2 md:col-span-3 lg:col-span-2 lg:pr-8">
-                <Logo/>
-                <p className="text-base leading-relaxed text-gray-600 mt-7">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+        <section className="py-4 bg-gray-50 sm:pt-4 lg:pt-8">
+        <div className="px-2 mx-auto sm:px-4 lg:px-6 max-w-7xl">
+            <div className="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-4 gap-x-6">
+                <div className="col-span-2 md:col-span-3 lg:col-span-2 lg:pr-4">
+                {/* <Logo/> */}
+                {/* <p className="text-base leading-relaxed text-gray-600 mt-7">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p> */}
 
                 <ul className="flex items-center space-x-3 mt-9">
 
@@ -107,7 +107,7 @@ const Footer = () => {
                 </ul>
             </div> */}
 
-            <div className="col-span-2 md:col-span-1 lg:col-span-2 lg:pl-8">
+            {/* <div className="col-span-2 md:col-span-1 lg:col-span-2 lg:pl-8">
                 <p className="text-sm font-semibold tracking-widest text-gray-400 uppercase">Subscribe to newsletter</p>
 
                 <form action="#" method="POST" className="mt-6">
@@ -118,10 +118,10 @@ const Footer = () => {
 
                     <button type="submit" className="inline-flex items-center justify-center px-6 py-4 mt-3 font-semibold text-white transition-all duration-200 bg-blue-600 rounded-md hover:bg-blue-700 focus:bg-blue-700">Subscribe</button>
                 </form>
-            </div>
+            </div> */}
         </div>
 
-        <hr className="mt-16 mb-10 border-gray-200" />
+        {/* <hr className="mt-16 mb-10 border-gray-200" /> */}
 
         <p className="text-sm text-center text-gray-600">© Copyright 2021, All Rights Reserved by Grootz</p>
     </div>
